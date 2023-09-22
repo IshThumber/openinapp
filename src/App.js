9377cf45e69c00d262810b93b65da3f4e8f13588
Ish Thumber <ishthumber343@gamil.com>
2023-09-22 14:02:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { useAuth0 } from "@auth0/auth0-react";
 const App = () => {
     const { isAuthenticated } = useAuth0();
 
+    console.log(isAuthenticated);
+
     return (
         <BrowserRouter>
             <Routes>
