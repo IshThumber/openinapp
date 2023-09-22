412ab17c9b312c24517a49ebf581163a8cf62ebc
Ish Thumber <ishthumber343@gamil.com>
2023-09-22 13:59:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Main from "./pages/Main";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
